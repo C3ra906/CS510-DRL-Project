e0dde505b48bbdb181624c8a4bc235c54e6a3425
Cera <ceoh@pdx.edu>
2022-05-20 03:34:38 -0700

    changed line 1733 "render" to "render_mode"

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -1730,7 +1730,7 @@ class Agent:
 
             if not self.training and self.render:
                 # Render the game-environment to screen.
-                self.env.render()
+                self.env.render_mode() #changed "render to render_mode
 
                 # Insert a small pause to slow down the game,
                 # making it easier to follow for human eyes.
